4d2c7ca86cab0212addf2bf7c78f436eb4730f4c
GiulioMastromartino <151667126+GiulioMastromartino@users.noreply.github.com>
2025-11-27 14:12:17 +0100

    memory fix

---
+++ Micro_ros_eth/microroseth/CM7/Core/Src/freertos.c
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* USER CODE BEGIN Includes */
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <uxr/client/transport.h>
+#include <rmw_microxrcedds_c/config.h>
+#include <rmw_microros/rmw_microros.h>
+#include <std_msgs/msg/int32.h>
+#include <microros_transports.h>
+#include "lwip.h"
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
+
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 3000 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+void StartDefaultTask(void *argument);
+extern void MX_LWIP_Init(void);
+
+void MX_FREERTOS_Init(void) {
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+}
+
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+
+  MX_LWIP_Init();
+  osDelay(3000);
+
+  rmw_uros_set_custom_transport(
+    false,
+    NULL,
+    cubemx_transport_open,
+    cubemx_transport_close,
+    cubemx_transport_write,
+    cubemx_transport_read
+  );
+
+  rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+  freeRTOS_allocator.allocate = microros_allocate;
+  freeRTOS_allocator.deallocate = microros_deallocate;
+  freeRTOS_allocator.reallocate = microros_reallocate;
+  freeRTOS_allocator.zero_allocate = microros_zero_allocate;
+
+  if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
+      while(1) { HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14); osDelay(100); }
+  }
+
+  rcl_publisher_t publisher;
+  std_msgs__msg__Int32 msg;
+  rclc_support_t support;
+  rcl_allocator_t allocator;
+  rcl_node_t node;
+
+  allocator = rcl_get_default_allocator();
+
+  rcl_ret_t ret = rclc_support_init(&support, 0, NULL, &allocator);
+
+  if (ret != RCL_RET_OK) {
+    while(1) {
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+        osDelay(100);
+    }
+  }
+
+  rclc_node_init_default(&node, "stm32_eth_node", "", &support);
+  rclc_publisher_init_default(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "eth_publisher");
+
+  msg.data = 0;
+
+  for(;;)
+  {
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+
+    ret = rcl_publish(&publisher, &msg, NULL);
+
+    if (ret != RCL_RET_OK) {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+    } else {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+    }
+
+    msg.data++;
+    osDelay(100);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Application */
+/* USER CODE END Application */
